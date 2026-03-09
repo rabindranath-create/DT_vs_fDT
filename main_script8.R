0161c280d42e4526aae7f43ea9e35eac825602a0
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2026-03-09 11:01:04 -0500

    Add files via upload

--- main_script8.R
+++ main_script8.R
@@ -2,18 +2,18 @@
 
 cat("Working directory:", getwd(), "\n")
 
-output_dir <- file.path(getwd(), "outputs/script10")
+output_dir <- file.path(getwd(), "outputs/script8")
 dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 cat("Created directory:", output_dir, "\n")
 
 source("DTvsfDT.R")
 
 
-obs_gen_para <- read.csv("patter_new/obstacle_10.csv")
+obs_gen_para <- read.csv("patter_new/obstacle_8.csv")
 
 
 the_result <- DT_Alg_save(obs_gen_para)
 
 
-file_name <- file.path(output_dir, paste0("DT10.rds"))
+file_name <- file.path(output_dir, paste0("DT8.rds"))
 saveRDS(the_result, file = file_name)
